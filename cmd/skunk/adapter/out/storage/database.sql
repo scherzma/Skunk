a02c8e0049a1f3a72650a365387196ea27b5f05b
M S <scherzma@protonmail.com>
2024-05-04 19:49:36 +0200

    Basic definition of the storage Port.
    Includes a SQL Schema for the database.

---
+++ cmd/skunk/adapter/out/storage/database.sql
@@ -0,0 +1,70 @@
+create table Chats
+(
+    chat_id varchar(1024) not null
+        constraint Chats_pk
+            primary key,
+    name    varchar(40)   not null
+);
+
+create table Peers
+(
+    peer_id    integer       not null
+        constraint Peers_pk
+            primary key autoincrement,
+    public_key varchar(1024) not null
+);
+
+create table ChatMembers
+(
+    chat_member_id integer       not null
+        constraint ChatMembers_pk
+            primary key autoincrement,
+    date           datetime      not null,
+    peer_id        varchar(1024) not null
+        constraint ChatMembers_Peers_peer_id_fk
+            references Peers
+            on update cascade,
+    chat_id        varchar(1024) not null
+        constraint ChatMembers_Chats_chat_id_fk
+            references Chats
+            on update cascade
+);
+
+create table Invitations
+(
+    invitation_id     integer       not null
+        constraint Invitations_pk
+            primary key autoincrement,
+    sender_peer_id    varchar(1024) not null
+        constraint Invitations_Peers_peer_id_fk
+            references Peers
+            on update cascade,
+    recipient_peer_id varchar(1024) not null
+        constraint Invitations_Peers_peer_id_fk_2
+            references Peers
+            on update cascade,
+    chat_id           varchar(1024) not null
+        constraint Invitations_Chats_chat_id_fk
+            references Chats
+            on update cascade,
+    date              datetime      not null,
+    invitation_status integer       not null
+);
+
+create table Messages
+(
+    message_id     integer       not null
+        constraint Messages_pk
+            primary key autoincrement,
+    content        text,
+    date           datetime      not null,
+    operation      integer       not null,
+    sender_peer_id varchar(1024) not null
+        constraint Messages_Peers_peer_id_fk
+            references Peers
+            on update cascade,
+    chat_id        varchar(1024) not null
+        constraint Messages_Chats_chat_id_fk
+            references Chats
+            on update cascade
+);
---
+++ cmd/skunk/application/port/store/storagePort.go
@@ -0,0 +1,64 @@
+package store
+
+import "github.com/scherzma/Skunk/cmd/skunk/application/port/network"
+
+type UserMessageStoragePort interface {
+	PeerSetUsername(peerId string, chatId string, username string) error
+}
+
+type ChatActionStoragePort interface {
+	PeerJoinedChat(peerId string, chatId string) error
+	PeerLeftChat(peerId string, chatId string, senderId string, date int64) error
+	ChatCreated(chatName string, chatId string) error
+}
+
+type ChatInvitationStoragePort interface {
+	InvitatedToChat(chatId string, chatName string, peers []string) error
+	PeerGotInvitedToChat(peerId string, chatId string) error
+	GetInvitations(peerId string) []string
+}
+
+type SyncStoragePort interface {
+	GetMissingInternalMessages(chatId string, inputMessageIDs []string) []string
+	GetMissingExternalMessages(chatId string, inputMessageIDs []string) []string
+}
+
+type NetworkMessageStoragePort interface {
+	StoreMessage(message network.Message) error
+	RetrieveMessage(messageId string) (network.Message, error)
+}
+
+type MessageType int
+
+const (
+	MESSAGE        MessageType = iota
+	File           MessageType = iota
+	UserJoined     MessageType = iota
+	UserLeft       MessageType = iota
+	UserWasInvited MessageType = iota
+)
+
+type ChatMessage struct {
+	Username  string
+	Content   string
+	MessageId int64
+	Timestamp int64
+}
+
+type Chat struct {
+	ChatId   string
+	ChatName string
+}
+
+type User struct {
+	UserId   string
+	Username string
+}
+
+type DisplayStoragePort interface {
+	GetChats() []Chat
+	GetUsername(peerId string) string
+	GetUsersInChat(chatId string) []User
+	GetPeers() []string
+	GetChatMessages(chatId string) []ChatMessage
+}
